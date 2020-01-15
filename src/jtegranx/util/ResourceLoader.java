dd1157279c8ba9e4ba1ea06737713d71cc6ec6c5
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2020-01-14 21:14:47 -0600

    Updated for new resources

--- src/jtegranx/util/ResourceLoader.java
+++ src/jtegranx/util/ResourceLoader.java
@@ -19,19 +19,84 @@ public class ResourceLoader {
 
     private static final int bufferSize = 1024;
 
+    // JTegraNX directory for resources
+    public static File jtegranxdir;
+
+    // Directory for memloader resources
+    public static File memloaderdir;
+
+    // Stored Resources
     public static File TegraRcmSmash;
+    public static File memloader_usb;
+    public static File uboot;
+    public static File[] ums_sd = {null, null, null};
+
+    public static final String dir = System.getProperty("user.dir");
 
     public static void loadResources() {
-        TegraRcmSmash = load("/jtegranx/res/TegraRcmSmash.exe");
-        TegraRcmSmash = rename(TegraRcmSmash, "TegraRcmSmash.exe");
-        TegraRcmSmash.deleteOnExit();
+        jtegranxdir = new File(dir + "\\jtegranx");
+        memloaderdir = new File(jtegranxdir.getAbsolutePath() + "\\memloader");
+
+        if (!jtegranxdir.exists()) {
+            jtegranxdir.mkdir();
+            TegraRcmSmash = extract("TegraRcmSmash.exe");
+        } else {
+            TegraRcmSmash = new File(jtegranxdir.getAbsolutePath() + "\\TegraRcmSmash.exe");
+
+            if (!TegraRcmSmash.exists()) {
+                TegraRcmSmash = extract("TegraRcmSmash.exe");
+            }
+        }
+
+        if (!memloaderdir.exists()) {
+            memloaderdir.mkdir();
+            memloader_usb = extract2("memloader_usb.bin");
+            uboot = extract2("u-boot.elf");
+            ums_sd[0] = extract2("ums_sd.ini");
+            ums_sd[1] = extract2("ums_sd.scr");
+            ums_sd[2] = extract2("ums_sd.scr.img");
+        } else {
+            memloader_usb = new File(memloaderdir.getAbsolutePath() + "\\memloader_usb.bin");
+            uboot = new File(memloaderdir.getAbsolutePath() + "\\u-boot.elf");
+            ums_sd[0] = new File(memloaderdir.getAbsolutePath() + "\\ums_sd.ini");
+            ums_sd[1] = new File(memloaderdir.getAbsolutePath() + "\\ums_sd.scr");
+            ums_sd[2] = new File(memloaderdir.getAbsolutePath() + "\\ums_sd.scr.img");
+
+            if (!memloader_usb.exists()) {
+                memloader_usb = extract2("memloader_usb.bin");
+            }
+
+            if (!uboot.exists()) {
+                uboot = extract2("u-boot.elf");
+            }
+
+            if (!ums_sd[0].exists()) {
+                ums_sd[0] = extract2("ums_sd.ini");
+            }
+
+            if (!ums_sd[1].exists()) {
+                ums_sd[1] = extract2("ums_sd.scr");
+            }
+
+            if (!ums_sd[2].exists()) {
+                ums_sd[2] = extract2("ums_sd.scr.img");
+            }
+        }
+    }
+
+    public static File extract(String file) {
+        return rename(load(file), dir + "\\jtegranx\\" + file);
+    }
+
+    public static File extract2(String file) {
+        return rename(load(file), dir + "\\jtegranx\\memloader\\" + file);
     }
 
     private static File load(String resource) {
         try {
-            input = ResourceLoader.class.getResourceAsStream(resource);
+            input = ResourceLoader.class.getResourceAsStream("/jtegranx/res/" + resource);
             bInput = new BufferedInputStream(input);
-            output = File.createTempFile(resource.substring(0, resource.indexOf(".")), resource.substring(resource.indexOf(".")));
+            output = File.createTempFile(resource, null);
             fos = new FileOutputStream(output);
             bos = new BufferedOutputStream(fos);
 
