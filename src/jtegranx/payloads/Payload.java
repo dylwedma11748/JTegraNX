e7915984e2c7bdbc1bc2a77ec495d1aba5901915
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2020-01-15 16:54:58 -0600

    License update

--- src/jtegranx/payloads/Payload.java
+++ src/jtegranx/payloads/Payload.java
@@ -1,3 +1,24 @@
+/*
+
+JTegraNX - Another GUI for TegraRcmSmash
+
+Copyright (C) 2020 Dylan Wedman
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
 package jtegranx.payloads;
 
 public class Payload {
@@ -19,7 +40,7 @@ public class Payload {
     public String getVersion() {
         return version;
     }
-    
+
     public String getDownloadURL() {
         return downloadURL;
     }
