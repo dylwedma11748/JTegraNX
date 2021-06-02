687eff7002e5cf97070da6b4b140d3e3c6301aa3
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2021-06-01 23:42:30 -0500

    Head start for adding portable mode

--- src/handlers/AlertHandler.java
+++ src/handlers/AlertHandler.java
@@ -21,41 +21,43 @@ with this program; if not, write to the Free Software Foundation, Inc.,
  */
 package handlers;
 
+import java.util.List;
 import java.util.Optional;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
 import ui.fx.JTegraNX;
 
 public class AlertHandler {
-
+    
     public static boolean showConfirmationDialog(String title, String headerText, String contentText) {
         Alert alert = new Alert(AlertType.CONFIRMATION);
-
+        
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
         alert.initOwner(JTegraNX.getStage());
-
+        
         Optional<ButtonType> result = alert.showAndWait();
-
+        
         return result.get() == ButtonType.OK;
     }
-
+    
     public static void showErrorMessage(String title, String headerText, String contentText) {
         Alert alert = new Alert(AlertType.ERROR);
-
+        
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
-
+        
         if (JTegraNX.getStage() != null) {
             alert.initOwner(JTegraNX.getStage());
         }
-
+        
         alert.showAndWait();
     }
-
+    
     public static Alert createAlert(String title, String headerText, String contentText) {
         Alert alert = new Alert(AlertType.NONE);
         alert.setTitle(title);
@@ -63,5 +65,21 @@ public class AlertHandler {
         alert.setContentText(contentText);
         alert.initOwner(JTegraNX.getStage());
         return alert;
+    }
+    
+    public static String createChoiceDialog(String title, String headerText, String contentText, List<String> choices) {
+        ChoiceDialog<String> dialog = new ChoiceDialog<>(choices.get(0), choices);
+        dialog.setTitle(title);
+        dialog.setHeaderText(headerText);
+        dialog.setContentText(contentText);
+        dialog.initOwner(JTegraNX.getStage());
+        
+        Optional<String> result = dialog.showAndWait();
+        
+        if (result.isPresent()) {
+            return result.get();
+        }
+        
+        return null;
     }
 }
